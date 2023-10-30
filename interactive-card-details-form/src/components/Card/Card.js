44689019fa735a5f2a2f0e1f3a981612413b0445
martinklestil <Martin.klestil@gmx.at>
2023-10-30 19:02:17 +0100

    i start with work on structure
    i make my first plan of structure and components

---
+++ interactive-card-details-form/src/components/Card/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = (porps) => {
+    return (
+        <div></div>
+    )
+}
+
+export default Card;
---
+++ interactive-card-details-form/src/components/Confirm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Confirm =(props) => {
+    return(
+        <div>
+            <img src="/images/icon-complete.svg"></img>
+            <h1>Thank you!</h1>
+            <p> We've added your card details Continue</p>
+            <button>Continue</button>
+        </div>
+    )
+}
+
+export default Confirm;
