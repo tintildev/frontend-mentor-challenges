8de7fdf99efc465309fc8bbea94ce6e05d0312a5
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2023-10-30 04:41:14 +0100

    add test data
    add test data

--- interactive-card-details-form/src/App.js
+++ interactive-card-details-form/src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Main></Main>
     </div>
   );
 }
---
+++ interactive-card-details-form/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer/Footer";
+
+const Main = (props) => {
+  return (
+    <div>
+      0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g. Jane
+      Appleseed Card Number e.g. 1234 5678 9123 0000 Exp. Date (MM/YY) MM YY CVC
+      e.g. 123 Confirm Thank you! We've added your card details Continue
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
