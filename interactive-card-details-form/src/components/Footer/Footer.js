a757ab56100aba2df2da2129a682e7a48919844d
martinklestil <Martin.klestil@gmx.at>
2023-11-06 19:05:57 +0100

    work on form style

--- interactive-card-details-form/src/components/Footer/Footer.js
+++ interactive-card-details-form/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => {
   return (
     <div class="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
+    Coded with love by <a href="#">Martin Klestil</a>.
   </div>
   )
 }
