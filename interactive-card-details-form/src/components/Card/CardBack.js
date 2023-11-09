c41c0c1b595615f1498a513b6dd1ae4de1d5dae5
martinklestil <Martin.klestil@gmx.at>
2023-11-09 18:10:15 +0100

    bugfix card back

--- interactive-card-details-form/src/components/Card/CardBack.js
+++ interactive-card-details-form/src/components/Card/CardBack.js
@@ -3,7 +3,8 @@ import "../sass/card.scss";
 
 const CardBack = (props) => {
     return (
-        <div className="card__Back" style={{backgroundImage: "url(/images/bg-card-back.png"}}>
+        <div className="card__Back" style={{backgroundImage: "url(/images/bg-card-back.png)"}}>
+            <p></p>
             <p className="card__Back--CVC">{props.value}</p>
         </div>
     )
