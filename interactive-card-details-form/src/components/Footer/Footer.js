4bed9b0ebc3d54efcfd157976a6108b760fdec2c
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2023-11-27 22:04:23 +0100

    update design

--- interactive-card-details-form/src/components/Footer/Footer.js
+++ interactive-card-details-form/src/components/Footer/Footer.js
@@ -2,11 +2,24 @@ import React from "react";
 
 const Footer = (props) => {
   return (
-    <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. 
-    Coded with love by <a href="#" target="_blank" rel="noopener noreferrer">Martin Klestil</a>.
-  </div>
-  )
-}
+    <footer>
+      <div class="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded with love by{" "}
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Martin Klestil
+        </a>
+        .
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
